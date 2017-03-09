ce952ca4017ee605932dfbce85554af721d112ad
Sam Chang <sam.chang@me.com>
2017-03-09 19:37:56 +0800

    update env.

--- config/mariadb.lua
+++ config/mariadb.lua
@@ -1,5 +1,5 @@
-maria_host = os.getenv("MARIA_HOST") or os.getenv("MYSQL_PORT_3306_TCP_ADDR") or "127.0.0.1"
-maria_port = os.getenv("MARIA_PORT") or os.getenv("MYSQL_PORT_3306_TCP_PORT")
+maria_host = os.getenv("MARIA_HOST") or os.getenv("MYSQL_PORT_3306_TCP_ADDR") or os.getenv("MARIADB_PORT_3306_TCP_ADDR") or "127.0.0.1"
+maria_port = os.getenv("MARIA_PORT") or os.getenv("MYSQL_PORT_3306_TCP_PORT") or os.getenv("MARIADB_PORT_3306_TCP_PORT")
 maria_database = os.getenv("MARIA_DATABASE_NAME") or "test"
 maria_user = os.getenv("MARIA_USERNAME") or "root"
 maria_passwd = os.getenv("MARIA_PASSWORD") or os.getenv("MYSQL_ENV_MYSQL_ROOT_PASSWORD")
