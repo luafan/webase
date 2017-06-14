535fe5b8025860a64b97126ecbf2d8fd23aa3616
Sam Chang <sam.chang@me.com>
2017-06-14 16:11:03 +0800

    update config.

--- config/mariadb.lua
+++ config/mariadb.lua
@@ -5,4 +5,4 @@ maria_user = os.getenv("MARIA_USERNAME") or "root"
 maria_passwd = os.getenv("MARIA_PASSWORD") or os.getenv("MYSQL_ENV_MYSQL_ROOT_PASSWORD") or os.getenv("MARIADB_ENV_MYSQL_ROOT_PASSWORD")
 maria_charset = os.getenv("MARIA_CHARSET") or "utf8"
 
-maria_pool_count = tonumber(os.getenv("MARIA_POOL_COUNT") or 10)
+maria_pool_size = tonumber(os.getenv("MARIA_POOL_COUNT") or 10)
